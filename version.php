2e202666654035022b295f710c848689b934d9ef
Bryan Holladay <holladay@longsight.com>
2016-01-21 11:39:01 -0500

    updating version

--- version.php
+++ version.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version =  2016012102;
+$plugin->version =  2016012200;
 $plugin->requires = 2014051200;
 $plugin->component = 'plagiarism_vericite';
 $plugin->maturity = MATURITY_STABLE;
